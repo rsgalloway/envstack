3d731a71fa59e8494abd0466920f034fa7176b10
Ryan Galloway <ryan@rsg.io>
2025-02-26 06:59:44 -0800

    bumps version to 0.8.2

--- lib/envstack/__init__.py
+++ lib/envstack/__init__.py
@@ -34,6 +34,6 @@ Stacked environment variable management system.
 """
 
 __prog__ = "envstack"
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 
 from envstack.env import clear, init, revert, save  # noqa: F401
