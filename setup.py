9cbfccf5e202ebf69a4980e038b05493bcce0d4e
Ryan Galloway <ryan@rsg.io>
2025-05-17 07:15:21 -0700

    bump setup version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open(os.path.join(here, "README.md")) as f:
 
 setup(
     name="envstack",
-    version="0.8.8",
+    version="0.8.9",
     description="Stacked environment variable management system",
     long_description=long_description,
     long_description_content_type="text/markdown",
